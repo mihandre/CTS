4ee0b431198c7932a3f0de8f297f8a18ba9ab473
andreimihai.sirbu <andreimihai.sirbu@gmail.com>
2021-03-30 14:41:29 +0300

    Build implementation

--- seminar5/src/ro/ase/csie/cts/g1094/dp/builder/SuperHero.java
+++ seminar5/src/ro/ase/csie/cts/g1094/dp/builder/SuperHero.java
@@ -2,4 +2,85 @@ package ro.ase.csie.cts.g1094.dp.builder;
 
 public class SuperHero {
 
+	String name;
+	int lifePoints;
+	boolean isVillain;
+	boolean isWounded;
+	
+	WeaponInterface leftWeapon;
+	WeaponInterface rightWeapon;
+	
+	String superPower;
+	String superSuperPower;
+	
+	public SuperHero() {
+		
+	}
+	
+	public SuperHero(String name, int lifePoints, boolean isVillain, boolean isWounded, WeaponInterface leftWeapon,
+			WeaponInterface rightWeapon, String superPower, String superSuperPower) {
+		super();
+		this.name = name;
+		this.lifePoints = lifePoints;
+		this.isVillain = isVillain;
+		this.isWounded = isWounded;
+		this.leftWeapon = leftWeapon;
+		this.rightWeapon = rightWeapon;
+		this.superPower = superPower;
+		this.superSuperPower = superSuperPower;
+	}
+	
+	public void takesAHit(int points) {
+		this.lifePoints -= points;
+		this.isWounded = true;
+	}
+	
+	public static class SuperHeroBuilder {
+		
+		private SuperHero superHero = null;
+		
+		public SuperHeroBuilder(String name, int lifePoints) {
+			superHero = new SuperHero();
+			superHero.name = name;
+			superHero.lifePoints = lifePoints;
+		}
+		
+		public SuperHeroBuilder isVillain() {
+			superHero.isVillain = true;
+			return this;
+		}
+		
+		public SuperHeroBuilder isWounded() {
+			superHero.isWounded = true;
+			return this;
+		}
+		
+		public SuperHeroBuilder addLeftWeapon(WeaponInterface weapon) {
+			superHero.leftWeapon = weapon;
+			return this;
+		}
+		
+		public SuperHeroBuilder addRightWeapon(WeaponInterface weapon) {
+			superHero.rightWeapon = weapon;
+			return this;
+		}
+		
+		public SuperHeroBuilder addSuperPower(String superPower) {
+			superHero.superPower = superPower;
+			return this;
+		}
+		
+		public SuperHeroBuilder addSuperSuperPower(String supersuperPower) {
+			superHero.superSuperPower = supersuperPower;
+			return this;
+		}
+		
+		
+		//don't forget to provide
+		public SuperHero build() {
+			return this;
+		}
+	}
+	
+	
 }
--- seminar5/src/ro/ase/csie/cts/g1094/dp/builder/TestBuilder.java
+++ seminar5/src/ro/ase/csie/cts/g1094/dp/builder/TestBuilder.java
@@ -3,8 +3,28 @@ package ro.ase.csie.cts.g1094.dp.builder;
 public class TestBuilder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//1. create the object
+		SuperHero superHero = new SuperHero();
+		//2. Init the object
+		//TO DO: don't forget
+//		superHero.name = "Superman";
+//		superHero.lifePoints = 100;
+//		superHero.superPower = "Can fly";
+//		superHero.superSuperPower= "Laser eyes";
+		
+//		SuperHero superHero =
+//				new SuperHero("Superman", 100, false, false, null, null, new Pistol(), "Laser Eyes");
 
+		SuperHero superman = new SuperHero.SuperHeroBuilder("Superman", 100)
+				.addSuperPower("fly")
+				.addLeftWeapon(new Pistol())
+				.build();
+		
+		SuperHero joker = new SuperHero.SuperHeroBuilder("Joker", 200)
+				.isVillain()
+				.addLeftWeapon(new Pistol())
+				.addRightWeapon(new Pistol())
+				.build();
 	}
 
 }
