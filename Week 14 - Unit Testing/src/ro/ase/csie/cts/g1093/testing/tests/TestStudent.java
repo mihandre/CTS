dc3baa7d0b40cad09d86614827b1e04e833b6bfb
andreimihai.sirbu <andreimihai.sirbu@gmail.com>
2021-06-03 15:40:06 +0300

    Test Suites, Test Categories

--- Week 14 - Unit Testing/src/ro/ase/csie/cts/g1093/testing/tests/TestStudent.java
+++ Week 14 - Unit Testing/src/ro/ase/csie/cts/g1093/testing/tests/TestStudent.java
@@ -2,33 +2,125 @@ package ro.ase.csie.cts.g1093.testing.tests;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import ro.ase.csie.cts.g1093.testing.exceptions.WrongAgeException;
+import ro.ase.csie.cts.g1093.testing.exceptions.WrongGradesException;
+import ro.ase.csie.cts.g1093.testing.exceptions.WrongNameException;
+import ro.ase.csie.cts.g1093.testing.models.Student;
 
 public class TestStudent {
 
+	//test fixture
+	static Student student = null;
+	static ArrayList<Integer> grades = null;
+	static int initialAge = 19;
+	static String initialName = "John Doe";
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		grades = new ArrayList<>();
+		grades.add(5);
+		grades.add(9);
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		grades.clear();
+		grades = null;
 	}
 
 	@Before
 	public void setUp() throws Exception {
+		student = new Student(initialName, initialAge, grades);
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		student = null;
 	}
 
+	@Ignore
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	public void testSetNameRightValue() throws WrongNameException {
+
+		String newName = "Alice";
+
+		student.setName(newName);
+
+		assertEquals("Testing with a right name", newName, student.getName());
+	}
+
+	@Test
+	public void testSetAgeRightValue() {
+		int newAge = initialAge;
+		try {
+			student.setAge(newAge);
+			assertEquals("Test for right value", newAge, student.getAge());
+		} catch (WrongAgeException e) {
+			fail("We got an exception for a right value");
+		}
+	}
+	
+	@Test
+	public void testSetAgeErrorCondition() {
+		int newAge = initialAge * -1;
+		
+		try {
+			student.setAge(newAge);
+			fail("We didn't get an exception for negative age");
+		} catch (WrongAgeException e) {
+			assertTrue(true);
+		}
+	}
+	
+	@Test(expected = WrongAgeException.class)
+	public void testSetAgeErrorConditionGreaterThanMax() throws WrongAgeException {
+		int newAge = Student.MAX_AGE + 1000;
+		student.setAge(newAge);
+	}
+	
+	@Test
+	public void testGetGradesAverageAscendingOrder() throws WrongGradesException {
+		ArrayList<Integer> sortedGrades = new ArrayList<>();
+		sortedGrades.add(7);
+		sortedGrades.add(8);
+		sortedGrades.add(9);
+		sortedGrades.add(10);
+		student.setGrades(sortedGrades);
+		
+		float expectedAverage = 8.5f;
+		float computedAverage = student.getGradesAverage();
+		
+		assertEquals("Testing with an ascending sorted set of grades",
+				expectedAverage, computedAverage, 0);
+	}
+	
+	@Test
+	public void testGetGradesAverageCardinalityZero() throws WrongGradesException {
+		ArrayList<Integer> gradesArrayList = new ArrayList<>();
+		int theGrade = 10;
+		gradesArrayList.add(theGrade);
+		
+		student.setGrades(gradesArrayList);
+		
+		float expectedAverage = theGrade;
+		float computedAverage = student.getGradesAverage();
+		
+		assertEquals("Testing with one grade",expectedAverage, computedAverage,0);
+		
+	}
+	
 }
--- Week 14 - Unit Testing/src/ro/ase/csie/cts/g1093/testing/tests/TestStudentNewTests.java
+++ Week 14 - Unit Testing/src/ro/ase/csie/cts/g1093/testing/tests/TestStudentNewTests.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 import org.junit.After;
@@ -12,10 +13,13 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 import ro.ase.csie.cts.g1093.testing.exceptions.WrongAgeException;
 import ro.ase.csie.cts.g1093.testing.exceptions.WrongGradesException;
 import ro.ase.csie.cts.g1093.testing.models.Student;
+import ro.ase.csie.cts.g1093.testing.tests.categories.ImportantTest;
+import ro.ase.csie.cts.g1093.testing.tests.categories.PerformanceTest;
 
 public class TestStudentNewTests {
 
@@ -25,6 +29,8 @@ public class TestStudentNewTests {
 	static String initialName;
 	static int initialAge;
 	static int initialNoGrades;
+	
+	static ArrayList<Integer> performanceGrades;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -37,6 +43,12 @@ public class TestStudentNewTests {
 		
 		for(int i = 0; i<initialNoGrades;i++) {
 			grades.add(random.nextInt(Student.MAX_GRADE)+1);
+		}
+		
+		performanceGrades = new ArrayList<>();
+		int noGrades = (int) 1e6;
+		for(int i = 0; i<initialNoGrades;i++) {
+			performanceGrades.add(random.nextInt(Student.MAX_GRADE)+1);
 		}
 	}
 
@@ -59,6 +71,7 @@ public class TestStudentNewTests {
 		fail("Not yet implemented");
 	}
 	
+	@Category(ImportantTest.class)
 	@Test(expected = WrongAgeException.class)
 	public void testSetAgeRangeMaxValue() throws WrongAgeException {
 		int newAge = Integer.MAX_VALUE;
@@ -104,5 +117,69 @@ public class TestStudentNewTests {
 		}
 		
 		assertArrayEquals("We do shallow copy",grades, studentGrades);
+	}
+	
+	@Category({ImportantTest.class, PerformanceTest.class})
+	@Test
+	public void testGetGradesAveragePerformance() throws WrongGradesException {
+		ArrayList<Integer> gradesArrayList = new ArrayList<Integer>();
+		int noGrades = (int) 1e6;
+		Random random = new Random();
+		for(int i = 0; i<noGrades;i++) {
+			grades.add(random.nextInt(Student.MAX_GRADE)+1);
+		}
+		
+		student.setGrades(grades);
+		
+		long tStart = System.currentTimeMillis();
+		student.getGradesAverage();
+		long tfinal = System.currentTimeMillis();
+		
+		long delta = tfinal - tStart;
+		long performanceLimit = 102;
+		if(delta<=performanceLimit) {
+			assertTrue(true);
+		}
+		else {
+			fail("Takes too long");
+		}
+	}
+	
+	@Category(PerformanceTest.class)
+	@Test(timeout = 102)
+	public void testGetGradesAveragePerformance2() throws WrongGradesException {
+		student.setGrades(performanceGrades);
+		student.getGradesAverage();
+	}
+	
+	@Test
+	public void testSetAgeInverse() throws WrongAgeException {
+		int newAge = initialAge+1;
+		student.setAge(newAge);
+		assertNotEquals("setAge is not changing the age value",initialAge, student.getAge());
+	}
+	
+	@Test
+	public void testGetMinGradeInverse() throws WrongGradesException {
+		student.setGrades(performanceGrades);
+		
+		int minGrade = student.getMinGrade();
+		for(int grade:performanceGrades) {
+			if(minGrade > grade) {
+				fail("The value is not min");
+			}
+		}
+		
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testGetMinCrossCheck() throws WrongGradesException {
+		student.setGrades(performanceGrades);
+		
+		int expectedMin = Collections.min(performanceGrades);
+		int computedMin = student.getMinGrade();
+		
+		assertEquals("Min is not correct",expectedMin, computedMin);
 	}
 }
